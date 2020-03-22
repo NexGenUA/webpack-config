48ee7978f1240c68b8ead5c92630eadfb6a5b3e3
NexGenUA <mr.rinus@gmail.com>
2020-03-22 20:48:00 +0200

    feat: add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,8 @@ module.exports = {
                 ],
             },
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
